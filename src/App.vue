c497d344729966b6a54082021f9cf2c0e974aafc
Lucas Laino <lucaslaino00@gmail.com>
2025-12-03 21:15:50 -0300

    feat(componente): Adiciona o componente Hero

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
   import Header from './components/Header/Header.vue'
+  import Hero from './components/Hero/Hero.vue'
 </script>
 
 <template>
   <Header />
+  <Hero />
 </template>
 
 <style scoped lang="scss"></style>
---
+++ src/components/Hero/Hero.vue
@@ -0,0 +1,40 @@
+<script setup></script>
+
+<template>
+    <section class="hero">
+        <article class="inspiraPixel-infos">
+            <h1>Onde a Paisagem Vira <span>Inspiração</span>.</h1>
+            <p>Explore paisagens, cores e luzes que inspiram novas histórias através da fotografia.</p>
+        </article>
+
+        <img src="../../assets/fotografo.jpg" alt="Imagem Fotografo">
+    </section>
+</template>
+
+<style scoped>
+.hero {
+    display: flex;
+    margin: 50px 0;
+    align-items: center;
+    justify-content: space-evenly;
+
+    h1 {
+        width: 400px;
+        font-size: 40px;
+
+        span {
+            font-size: 50px;
+            color: #E1306C;
+        }
+    }
+
+    p {
+        width: 680px;
+        font-size: 20px;
+    }
+
+    img {
+        width: 25%;
+    }
+}
+</style>
